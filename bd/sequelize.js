fbd35c48d4c7f0a24024b6cf4d8907509a027ac6
oumar adje <adjeoumar@gmail.com>
2023-05-04 17:28:54 +0200

    port bloc

--- bd/sequelize.js
+++ bd/sequelize.js
@@ -7,7 +7,7 @@ const modelCatalogue= require('../models/Cataogue')
 //  database connexion
 const sequelize = new Sequelize(process.env.DB_NAME || 'couture-back', process.env.DB_USER || 'root', process.env.DB_PASSWORD ||'root', {
    host: process.env.DB_HOST ||'localhost',
-   port: process.env.DB_PORT || 3306,
+   port: 7963 ,
    dialect: 'mysql',
    logging: console.log 
 })
